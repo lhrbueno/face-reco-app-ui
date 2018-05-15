4bad44c65f3e80eb16045fd97f0c78cfcf5d146f
lhrbueno <luishenriquebueno@gmail.com>
2018-05-15 00:36:56 -0300

    Added face recognition functionality; work for face only

--- src/App.js
+++ src/App.js
@@ -1,18 +1,105 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Navigation from './components/Navigation/Navigation';
+import Logo from './components/Logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
+import Particles from 'react-particles-js';
+import Clarifai from 'clarifai';
+import Signin from './components/Signin/Signin';
+
+import 'tachyons';
 import './App.css';
 
+const app = new Clarifai.App({
+  apiKey: 'ec7f6e50d4034297a14c9f4231a0a0b8'
+});
+
+const particlesParams = {
+  particles: {
+    number: {
+      value: 187,
+      density: {
+        enable: true,
+        value_area: 900
+      }
+    },
+    move: {
+      enable: true,
+      speed: 7,
+    }
+  }
+};
+
 class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+      imageUrl: '',
+      box: {},
+      route: 'signin'
+    }
+  }
+
+  calculateFaceLocation = (data) => {
+    const clarifaiImage = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputImage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return {
+      topRow: clarifaiImage.top_row * height,
+      rightCol: width - (clarifaiImage.right_col * width),
+      bottomRow: height - (clarifaiImage.bottom_row * height),
+      leftCol: clarifaiImage.left_col * width
+    }
+  }
+
+  displayFaceBox = (box) => {
+    this.setState({ box: box });
+  }
+
+  onInputChange = (e) => {
+    this.setState({ input: e.target.value });
+  }
+
+  onButtonSubmit = () => {
+    this.setState({ imageUrl: this.state.input });
+
+    app.models
+      .predict(
+        Clarifai.FACE_DETECT_MODEL, 
+        this.state.input
+      )
+      .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+      .catch(err => console.log(err));
+  }
+
+  onRouteChange = (route) => {
+    this.setState({ route: route });
+  }
+
   render() {
+    const { imageUrl, box } = this.state;
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Particles params={particlesParams} className="particles"/>
+        <Navigation onRouteChange={this.onRouteChange} />
+        
+        { 
+          this.state.route === 'signin'
+          ? <Signin onRouteChange={this.onRouteChange} />
+          : <div>
+            <Logo />
+            <Rank />
+            <ImageLinkForm 
+              onInputChange={this.onInputChange} 
+              onButtonSubmit={this.onButtonSubmit} />
+            <FaceRecognition imageUrl={imageUrl} box={box} />
+          </div>
+        }
       </div>
     );
   }
