321171b280d8ee2c2ac9e75fa2fe5bef26b3a823
lhrbueno <luishenriquebueno@gmail.com>
2018-05-16 16:05:29 -0300

    Added isLoggedIn check. Added SignIn and Register menu

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,11 +1,14 @@
 import React from 'react';
 
-const Navigaton = ({ onRouteChange }) => {
-  return (
-    <nav style={{display: 'flex', justifyContent: 'flex-end'}}>
-      <p onClick={() => onRouteChange('signin')} className='f3 link dim black underline pa3 pointer'>Sign out</p>
-    </nav>
-  );
+const Navigaton = ({ onRouteChange, isLoggedIn }) => {
+  return isLoggedIn
+    ? <nav style={{display: 'flex', justifyContent: 'flex-end'}}>
+        <p onClick={() => onRouteChange('signin')} className='f3 link dim black underline pa3 pointer'>Sign out</p>
+      </nav>
+    : <nav style={{display: 'flex', justifyContent: 'flex-end'}}>
+        <p onClick={() => onRouteChange('signin')} className='f3 link dim black underline pa3 pointer'>Sign in</p>
+        <p onClick={() => onRouteChange('register')} className='f3 link dim black underline pa3 pointer'>Register</p>
+      </nav>
 }
 
 export default Navigaton;
