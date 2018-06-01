74433300b211df003fe45574de1df9845d482a2a
lhrbueno <luishenriquebueno@gmail.com>
2018-06-01 17:49:22 -0300

    Added React Link

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-const Navigaton = ({ onRouteChange, isLoggedIn }) => {
+const Navigaton = ({ isLoggedIn, onSignOut }) => {
   return isLoggedIn
     ? <nav style={{display: 'flex', justifyContent: 'flex-end'}}>
-        <p onClick={() => onRouteChange('signin')} className='f3 link dim black underline pa3 pointer'>Sign out</p>
+        <div onClick={() => onSignOut()} className='f3 link dim black underline pa3 pointer'><Link to='/signin'>Sign out</Link></div>
       </nav>
     : <nav style={{display: 'flex', justifyContent: 'flex-end'}}>
-        <p onClick={() => onRouteChange('signin')} className='f3 link dim black underline pa3 pointer'>Sign in</p>
-        <p onClick={() => onRouteChange('register')} className='f3 link dim black underline pa3 pointer'>Register</p>
+        <div className='f3 link dim black underline pa3 pointer'><Link to='/signin'>Sign in</Link></div>
+        <div className='f3 link dim black underline pa3 pointer'><Link to='/register'>Register</Link></div>
       </nav>
 }
 
