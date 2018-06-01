b2b0ea341abd15f44469646b9490d109a82202f0
lhrbueno <luishenriquebueno@gmail.com>
2018-06-01 17:54:35 -0300

    Added React router and implemented routes
    Removed Logo, Rank and ImageLinkForm components.\n Removed faceRecognition functions.\n Added onSignIn and onSignOut mock

--- src/App.js
+++ src/App.js
@@ -1,20 +1,14 @@
 import React, { Component } from 'react';
-import Navigation from './components/Navigation/Navigation';
-import Logo from './components/Logo/Logo';
-import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
-import Rank from './components/Rank/Rank';
-import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import Particles from 'react-particles-js';
-import Clarifai from 'clarifai';
+import { Route, Switch, Redirect } from 'react-router-dom';
+
+import Navigation from './components/Navigation/Navigation';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import Signin from './components/Signin/Signin';
+import Register from './components/Register/Register';
 
 import 'tachyons';
 import './App.css';
-import Register from './components/Register/Register';
-
-const app = new Clarifai.App({
-  apiKey: 'ec7f6e50d4034297a14c9f4231a0a0b8'
-});
 
 const particlesParams = {
   particles: {
@@ -37,77 +31,32 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      input: '',
-      imageUrl: '',
-      box: {},
-      route: 'signin',
       isLoggedIn: false
     }
   }
 
-  calculateFaceLocation = (data) => {
-    const clarifaiImage = data.outputs[0].data.regions[0].region_info.bounding_box;
-    const image = document.getElementById('inputImage');
-    const width = Number(image.width);
-    const height = Number(image.height);
-    return {
-      topRow: clarifaiImage.top_row * height,
-      rightCol: width - (clarifaiImage.right_col * width),
-      bottomRow: height - (clarifaiImage.bottom_row * height),
-      leftCol: clarifaiImage.left_col * width
-    }
+  onSignIn = () => {
+    this.setState({ isLoggedIn: true });
   }
 
-  displayFaceBox = (box) => {
-    this.setState({ box: box });
-  }
-
-  onInputChange = (e) => {
-    this.setState({ input: e.target.value });
-  }
-
-  onButtonSubmit = () => {
-    this.setState({ imageUrl: this.state.input });
-
-    app.models
-      .predict(
-        Clarifai.FACE_DETECT_MODEL, 
-        this.state.input
-      )
-      .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
-      .catch(err => console.log(err));
-  }
-
-  onRouteChange = (route) => {
-    route === 'home' ? this.setState({ isLoggedIn: true }) : this.setState({ isLoggedIn: false });
-    this.setState({ route: route });
+  onSignOut = () => {
+    this.setState({ isLoggedIn: false });
   }
 
   render() {
-    const { imageUrl, box, route, isLoggedIn } = this.state;
+    const { isLoggedIn } = this.state;
 
     return (
       <div className="App">
         <Particles params={particlesParams} className="particles"/>
-        <Navigation onRouteChange={this.onRouteChange} isLoggedIn={isLoggedIn} />
-        
-        { 
-          route === 'home'
-          ?
-          <div>
-            <Logo />
-            <Rank />
-            <ImageLinkForm 
-              onInputChange={this.onInputChange} 
-              onButtonSubmit={this.onButtonSubmit} />
-            <FaceRecognition imageUrl={imageUrl} box={box} />
-          </div>
-          : (
-            route === 'signin'
-            ? <Signin onRouteChange={this.onRouteChange} />
-            : <Register onRouteChange={this.onRouteChange} />
-          )
-        }
+        <Navigation isLoggedIn={isLoggedIn} onSignOut={this.onSignOut} />
+    
+        <Switch>
+          <Route exact path='/' render={() => <Redirect to='/signin' /> } /> 
+          <Route exact path='/signin' render={() => <Signin onSignIn={this.onSignIn} />} />
+          <Route exact path='/register' component={Register} />
+          <Route exact path='/dashboard' component={FaceRecognition} />
+        </Switch>
       </div>
     );
   }
