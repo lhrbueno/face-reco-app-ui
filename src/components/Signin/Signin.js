11c0eaef31da36b1882cb4897c52e48d2e374ab2
lhrbueno <luishenriquebueno@gmail.com>
2018-07-29 17:10:43 -0300

    Changed Signin to class. Added Signin logic. Changed route from dashboard to profile/id

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -1,38 +1,86 @@
-import React from 'react';
-import { Route, Link } from 'react-router-dom';
+import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
 
-const Signin = ({ onSignIn }) => {
-  return(
-    <div className="br2 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
-      <main className="pa4 black-80">
-        <div className="measure">
-          <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
-            <legend className="f1 fw6 ph0 mh0">Sign In</legend>
-            <div className="mt3">
-              <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
-              <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="email" name="email-address"  id="email-address" />
-            </div>
-            <div className="mv3">
-              <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
-              <input className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="password" name="password"  id="password" />
-            </div>
-          </fieldset>
-          <div className="">
-            <Link to='/dashboard'>
+class Signin extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: ''
+    }
+  }
+
+  onEmailChange = (e) => {
+    this.setState({ email: e.target.value });
+  }
+
+  onPasswordChange = (e) => {
+    this.setState({ password: e.target.value });
+  }
+
+  onSubmit = () => {
+    const { email, password } = this.state;
+    fetch('http://localhost:3003/signin', {
+      method: 'post',
+      headers: { 'Content-Type': 'application/json'},
+      body: JSON.stringify({
+        email: email,
+        password: password
+      })
+    })
+      .then(res => res.json())
+      .then(data => {
+        if (data.user.id) { 
+          this.props.onSignIn();
+          this.props.loadUser(data.user);
+          this.props.history.push(`/profile/${data.user.id}`);
+        } else {
+          this.props.history.push('/signin');
+        }
+      });
+  }
+
+  render() {
+    return(
+      <div className="br2 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
+        <main className="pa4 black-80">
+          <div className="measure">
+            <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
+              <legend className="f1 fw6 ph0 mh0">Sign In</legend>
+              <div className="mt3">
+                <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
+                <input 
+                  className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" 
+                  type="email" 
+                  name="email-address" 
+                  id="email-address" 
+                  onChange={ this.onEmailChange }
+                />
+              </div>
+              <div className="mv3">
+                <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
+                <input 
+                  className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" 
+                  type="password" 
+                  name="password" 
+                  id="password" 
+                  onChange={ this.onPasswordChange }
+                />
+              </div>
+            </fieldset>
+            <div className="">
               <input 
                 className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" 
                 type="submit" 
                 value="Sign in" 
-                onClick={() => onSignIn()}
+                onClick={() => this.onSubmit()}
               />
-            </Link>
-
-            <Route exact path='/dashboard' />
+            </div>
           </div>
-        </div>
-      </main>
-    </div>
-  );
+        </main>
+      </div>
+    );
+  }
 }
 
-export default Signin;
+export default withRouter(Signin);
