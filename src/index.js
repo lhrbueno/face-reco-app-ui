bd8958ade26baffbce07cc1038aa0a6ed06471a3
lhrbueno <luishenriquebueno@gmail.com>
2018-11-10 17:13:11 -0200

    Updated create-react-app to version 2.1

--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom'
-import registerServiceWorker from './registerServiceWorker';
+import * as serviceWorker from './registerServiceWorker';
 
 ReactDOM.render(
   <Router>
     <App />
   </Router>, document.getElementById('root'));
-registerServiceWorker();
+
+serviceWorker();
