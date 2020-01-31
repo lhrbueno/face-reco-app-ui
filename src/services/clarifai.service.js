8aba9d4bf9c2900804059e5a8a548d0164044b81
lhrbueno <luishenriquebueno@gmail.com>
2020-01-31 01:18:17 -0300

    Changed service to call backend API

--- src/services/clarifai.service.js
+++ src/services/clarifai.service.js
@@ -1,14 +1,15 @@
-import ClarifaiConfig from '../config/clarifai.config';
-
 const ClarifaiService = {
-
-  getFaceRecognition: (input) => {
-    return ClarifaiConfig
-      .app
-      .models
-      .predict(ClarifaiConfig.FACE_DETECTION, input);
+  getFaceRecognition: imageUrl => {
+    return fetch('http://localhost:3003/api/image-recognition', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        imageUrl
+      })
+    })
+      .then(res => res.json())
+      .catch(err => console.log(err));
   }
-
 };
 
 export default ClarifaiService;
